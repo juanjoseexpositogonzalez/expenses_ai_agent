70ec5e30ad067af01c549e77f5f84a1d3d20176d
Juan José Expósito González <expositogonzalez.juanjose@gmail.com>
2025-09-24 22:44:07 +0200

    Added ExpenseCreationError exception

--- src/expenses_ai_agent/storage/exceptions.py
+++ src/expenses_ai_agent/storage/exceptions.py
@@ -5,6 +5,13 @@ class CategoryNotFoundError(Exception):
         self.message = message
 
 
+class ExpenseCreationError(Exception):
+    """Raised when there is an error creating an expense."""
+
+    def __init__(self, message: str = "Error creating expense"):
+        self.message = message
+
+
 class ExpenseNotFoundError(Exception):
     """Raised when an expense is not found in the database."""
 
