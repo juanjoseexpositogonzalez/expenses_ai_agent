61bb0174488888642f6e56537efe46ff5de49c34
Juan José Expósito González <expositogonzalez.juanjose@gmail.com>
2025-09-25 09:29:50 +0200

    Added format to decimal numbers in currency script

--- scripts/week1/currency_test.py
+++ scripts/week1/currency_test.py
@@ -1,5 +1,5 @@
 import logging
-from decimal import Decimal
+from decimal import Decimal, getcontext
 
 from expenses_ai_agent.utils.currency import Currency, convert_currency
 
@@ -8,16 +8,17 @@ logger = logging.getLogger(__name__)
 
 
 def main() -> None:
+    getcontext().prec = 2
     amount = Decimal(100)
     from_currency: Currency = Currency.USD
     to_currency: Currency = Currency.GBP
     logger.info("Running currency conversion tests...")
-    logger.info("Converting %s %s to %s", amount, from_currency, to_currency)
+    logger.info("Converting %d %s to %s", amount, from_currency, to_currency)
     converted_amount = convert_currency(amount, from_currency, to_currency)
     assert isinstance(converted_amount, Decimal)
     assert converted_amount > 0
     logger.info(
-        "Conversion successful: %s %s = %s %s",
+        "Conversion successful: %.2f %s = %.2f %s",
         amount,
         from_currency,
         converted_amount,
